cff962be03240423b5206c02faaa91605d81cb3f
Robbie <robbiejohnwallace@yahoo.co.uk>
2021-12-14 10:56:06 +0000

    Finished Part1 of day 9 Just about to start Part2.

--- src/day09.rs
+++ src/day09.rs
@@ -5,18 +5,58 @@ use std::io::{BufReader, BufRead};
 struct Heightmap {
     width: u32,
     height: u32,
-    store: Vec<i32>,
+    store: Vec<Vec<i32>>,
 }
+
+const ADJ: [[i32; 2]; 4] = [[0, 1], [0, -1], [1, 0], [-1, 0]];
 
 
 pub fn main(file_name: String) -> std::io::Result<()> {
     // Read file 
-    let heightmap = read_heightmap(file_name)?;
+    let hm = read_heightmap(file_name)?;
+
+    // Find low points 
+    let mut result = 0;
+
+    for x in 0..hm.width {
+        'outer: for y  in 0..hm.height {
+            for offset in ADJ {
+                let off_x:i32 = (x as i32) + offset[0];
+                let off_y:i32 = (y as i32) + offset[1];
+
+                if off_x < 0 || off_x >= hm.width as i32 || off_y < 0 ||  off_y >= hm.height as i32 {
+                    continue;
+                }
+
+                if hm.store[y as usize][x as usize] >= hm.store[off_y as usize][off_x as usize] {
+                    continue 'outer;
+                }
+            }
+
+            // Found minimum
+            //result += hm.store[y as usize][x as usize] + 1;
+
+            // Find the size of this basin 
+            let basin_size = get_basin_size(&hm, x as u32, y as u32);
+
+            println!("{} : {}", hm.store[y as usize][x as usize], basin_size);
+
+            result = result * basin_size;
+        }
+    }
+
+    println!("{}", result);
     
 
     // Finished 
     Ok(())
 }
+
+
+fn get_basin_size(hm: &Heightmap, x: u32, y: u32) -> u32 {
+    0
+}
+
 
 
 fn read_heightmap(file_name: String) -> std::io::Result<Heightmap>{
@@ -30,6 +70,7 @@ fn read_heightmap(file_name: String) -> std::io::Result<Heightmap>{
     let br = BufReader::new(file);
 
     for line in br.lines(){
+        let mut line_store = Vec::new();
         let line = line?;
 
         width = line.len();
@@ -37,10 +78,12 @@ fn read_heightmap(file_name: String) -> std::io::Result<Heightmap>{
 
         for c in line.chars() {
             match c.to_digit(10) {
-                Some(a) => store.push(a as i32),
+                Some(a) => line_store.push(a as i32),
                 None => ()
             }
         }
+
+        store.push(line_store);
     }
 
     // Finished 
