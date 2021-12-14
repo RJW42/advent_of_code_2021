8c0e7ef79e8e2a81dbcde44a959cd0756743493c
Robbie <robbiejohnwallace@yahoo.co.uk>
2021-12-14 10:39:56 +0000

    Did Some Work on Day 8 Part 2.

--- src/day08.rs
+++ src/day08.rs
@@ -5,28 +5,105 @@ use std::io::{BufReader, BufRead};
 struct SignalEntry {
     signal_code: Vec<[bool; 7]>, // 10 
     signal_value: Vec<[bool; 7]>, // 4
+    combined: Vec<[bool; 7]>, // 14
 }
 
 
 pub fn main(file_name: String) -> std::io::Result<()> {
     // Get segment codes 
     let signals = get_codes(&file_name)?;
-    let mut result = 0;
 
     // Calc result 
     for signal in signals {
-        for value in signal.signal_value {
-            result += match value.iter().fold(0, |sum, val| sum + (*val as u32)) {
-                7 => 1, // 8
-                3 => 1, // 7
-                4 => 1, // 4
-                2 => 1, // 1
-                _ => 0
+        // Figure out how to display each number 
+        let mut decoder = [[false; 7]; 10];
+        let mut remaning = Vec::<[bool; 7]>::new();
+
+        let mut display = [0; 10];
+        /* 0 -> Top
+           1 -> Top Left
+           2 -> Top Right
+           3 -> Middle
+           4 -> Bottom Left
+           5 -> Bottom Right
+           6 -> Bottom 
+        */
+
+        for value in signal.combined {
+            match (&value).iter().fold(0, |sum, val| sum + (*val as u32)) {
+                7 => decoder[8] = value, // 8
+                3 => decoder[7] = value, // 7
+                4 => decoder[4] = value, // 4
+                2 => decoder[1] = value, // 1
+                _ => remaning.push(value)
             };
         }
-    }
 
-    println!("{}", result);
+        // Get Top Value from diference between 7,1
+        for i in 0..7 {
+            if (decoder[7][i] == true) && (decoder[1][i] == false) {
+                display[0] = i;
+                break;
+            }
+        }
+
+        // Get Bottom value from value of size 5 with diff 1 on set of elements in (4 + Top).
+        // Also Gets Values 2, 9 
+        let mut set_4_top: [bool; 7] = decoder[4].clone();
+                set_4_top[display[0]] = true;
+        let mut new_remaning = Vec::<[bool; 7]>::new();
+
+
+        for val in remaning {
+            let size: u32 = (&val).iter().fold(0, |sum, v| sum + (*v as u32));
+            let diff: u32 = (&val).iter().zip((&set_4_top).iter()).fold(0, |sum, (a, b)| sum + (*a == !*b) as u32);
+            print!("{}:{}, ", size, diff);
+
+            if size == 6 && diff == 3 {
+                // Got 9. Extract Bottom Value 
+                decoder[9] = val;
+                continue;
+            }
+
+            if size == 5 && diff == 4 {
+                // Got 2
+                decoder[2] = val;
+
+                continue;
+            }
+
+            new_remaning.push(val);
+        }
+        println!();
+
+        remaning = new_remaning;
+
+
+        // Get 0, 3, 5, 6 Using the size of the set and its difference with 1 
+        for val in remaning {
+            let size: u32 = (&val).iter().fold(0, |sum, val| sum + (*val as u32));
+            let diff: u32 = (&val).iter().zip((&decoder[1]).iter()).fold(0, |sum, (a, b)| sum + (*b && !*a) as u32);
+
+            match (size, diff) {
+                (5, 0) => decoder[3] = val, // 3
+                (5, 1) => decoder[5] = val, // 5
+                (6, 0) => decoder[0] = val, // 0
+                (6, 1) => decoder[6] = val, // 6
+                _ => (),
+            };
+        }
+
+        // Get The Value of the output 
+        for digit in signal.signal_value {
+            for (i, decode) in (&decoder).iter().enumerate() {
+                if &digit[..] == &decode[..] {
+                    print!("{}", i);
+                }
+            }
+            print!(" ");
+        }
+        println!();
+    }
 
     // Finished 
     Ok(())
@@ -49,11 +126,14 @@ fn get_codes(file_name: &String) -> std::io::Result<Vec<SignalEntry>> {
         // Parse code and value 
         let codes = get_code(code);
         let values = get_code(value);
+        let mut combined = values.to_vec();
+        combined.extend(codes.to_vec());
 
         // Add mew signal entry to output 
         signals.push(SignalEntry {
             signal_code: codes,
-            signal_value: values
+            signal_value: values,
+            combined: combined
         });
     }
 
